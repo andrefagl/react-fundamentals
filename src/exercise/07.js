f09075a7f9a7c997ce5ab4bf32c14d465219480e
Filipe Lima <filipe.lima@farfetch.com>
2021-01-09 17:40:13 +0000

    Course completion

--- src/exercise/07.js
+++ src/exercise/07.js
@@ -1,41 +1,45 @@
 // Rendering Lists
 // http://localhost:3000/isolated/exercise/07.js
 
-import * as React from 'react'
+import React from 'react'
 
 const allItems = [
-  {id: 'apple', value: '🍎 apple'},
-  {id: 'orange', value: '🍊 orange'},
-  {id: 'grape', value: '🍇 grape'},
-  {id: 'pear', value: '🍐 pear'},
+  {id: 'a', value: 'apple'},
+  {id: 'o', value: 'orange'},
+  {id: 'g', value: 'grape'},
+  {id: 'p', value: 'pear'},
 ]
 
 function App() {
   const [items, setItems] = React.useState(allItems)
 
   function addItem() {
-    setItems([
-      ...items,
-      allItems.find(i => !items.map(({id}) => id).includes(i.id)),
-    ])
+    setItems([...items, allItems.find(i => !items.includes(i))])
   }
 
   function removeItem(item) {
-    setItems(items.filter(i => i.id !== item.id))
+    setItems(items.filter(i => i !== item))
   }
 
   return (
-    <div className="keys">
+    <div
+      style={{
+        height: 200,
+        width: 400,
+        backgroundColor: '#eee',
+        borderRadius: 4,
+        padding: 20,
+      }}
+    >
       <button disabled={items.length >= allItems.length} onClick={addItem}>
         add item
       </button>
       <ul style={{listStyle: 'none', paddingLeft: 0}}>
         {items.map(item => (
-          // 🐨 add a key prop to the <li> below. Set it to item.id
-          <li>
+          <li key={item.id}>
             <button onClick={() => removeItem(item)}>remove</button>{' '}
-            <label htmlFor={`${item.id}-input`}>{item.value}</label>{' '}
-            <input id={`${item.id}-input`} defaultValue={item.value} />
+            <label htmlFor={`${item.value}-input`}>{item.value}</label>{' '}
+            <input id={`${item.value}-input`} defaultValue={item.value} />
           </li>
         ))}
       </ul>
